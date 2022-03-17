ff4f5ca356a1f85bc0c07478e18a3fa4986cb188
Your Name <your@email.com>
2022-03-17 05:15:13 -0400

    Update students.js and teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -45,18 +45,56 @@ const pool = new Pool({
 // Query Parameters
 // Let's make our application a little bit more dynamic. We will allow a user to specify a cohort name and
 // the limit. So someone should be able to run the application with the following command:
+
 // node students.js FEB 2
+
 // Where FEB is the cohort name and 2 is the maximum number of results, and the results would be:
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-});
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3]};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// });
+
+// Stephanie Wolff has an id of 2 and was in the FEB12 cohort
+// Stan Miller has an id of 3 and was in the FEB12 cohort
+
+// // Update students.js and teachers.js to use parameterized queries.
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+// Store all potentially malicious values in an array.
+const values = [`%${process.argv[2]}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
+    // console.log(res.rows);
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  });
+
+
+  // console.log(res.rows); from the above returned promise... getting the key names from the queryString variable
+  // [
+  //   { student_id: 2, name: 'Stephanie Wolff', cohort: 'FEB12' },
+  //   { student_id: 3, name: 'Stan Miller', cohort: 'FEB12' }
+  // ]
+
+// Stephanie Wolff has an id of 2 and was in the FEB12 cohort
+// Stan Miller has an id of 3 and was in the FEB12 cohort
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -40,19 +40,29 @@ const pool = new Pool({
 // WHERE cohorts.name = 'JUL02'
 // ORDER BY teacher;
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER BY teacher;
-`)
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
+
+const cohortName = process.argv[2];
+
+// Store all potentially malicious values in an array.
+const values = [cohortName];
+// console.log("values::", values);// values:: [ 'JUL02' ]
+
+// node teachers.js JUL02 `${cohortName}`// 0 1 2 --> 2 elem in process.argv
+
+
+// pool.query(queryString, values);//just hangs... not returning the promise i guess?
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
     });
   });
-
-
